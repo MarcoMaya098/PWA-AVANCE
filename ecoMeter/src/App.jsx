bb56e79eac2f27d24a37de334b62aef78b7f5b74
Marco <marco.maya098@gmail.com>
2024-03-15 00:15:19 -0700

    commit avance

--- ecoMeter/src/App.jsx
+++ ecoMeter/src/App.jsx
@@ -1,42 +1,35 @@
-import { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Container } from "@mui/material";
-// import { StepContextProvider } from './context/StepContext.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// import TaskForm from "./components/TaskForm";
-// import TasksList from "./components/TasksList";
-// import ClientForm from './components/examples/ClientsForm';
-// import ClientList from './components/examples/ClientList';
-import Login from './components/Login';
-import SingUp from './components/SingUp';
-import Dashboard from './components/dashboard/Dashboard';
-
-// import PrivateRoute from "./PrivateRoute";
-// import Register from "./components/Register";
-// import { Link } from "react-router-dom";
-// import Menu from "./components/Navbar";
- 
 function App() {
-  // const isAuthenticated = localStorage.getItem('jwt');
+  const [count, setCount] = useState(0)
+
   return (
-    <Fragment>
-      <Container>
-      {/* <StepContextProvider> */}
-        <Routes>
-          <Route index path="/" element={<Login />} />
-          <Route index path="/signup" element={<SingUp />} />
-          <Route index path="/dashboard" element={<Dashboard />} />
-
-          
-          {/* <Route path="/tasks" element={<TasksList />} />
-          <Route path="/tasks/new" element={<TaskForm />} />
-          <Route path="/tasks/:id/edit" element={<TaskForm />} /> */}
-        </Routes>
-        {/* </StepContextProvider> */}
-      </Container>
-    </Fragment>
-
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- ecoMeter/src/main.jsx
+++ ecoMeter/src/main.jsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <StepContext>
-//       <React.StrictMode>
-//          <App />
-//       </React.StrictMode>
-//     </StepContext>
-//   </BrowserRouter>
-
-// );
+    <App />
+  </React.StrictMode>,
+)
